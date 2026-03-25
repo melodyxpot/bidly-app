80156ec225db62ab70fa7276e8b1e6378243c921
melodyxpot <melodyxpot@gmail.com>
2026-03-26 03:55:18 +0800

    fix: update server for cors policy

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -13,7 +13,7 @@ import { profileRouter } from "./routes/profile"
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(cors())
+app.use(cors({ origin: "*" }))
 app.use(express.json({ limit: "10mb" }))
 
 app.use("/api/auth", authRouter)
